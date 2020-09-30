b552c9ab767e48c9ce762744a46db537cb9cc1c1
Giuseppe <giuseppe.ilsami@gmail.com>
2020-09-30 22:26:47 +0200

    aggiornamento dati

--- assets/js/dataApp.js
+++ assets/js/dataApp.js
@@ -4,70 +4,70 @@ export let datiFilm = [
         media: [
             {
                 titolo: "Matrix",
-                descrizione: "Descrizione fantascienza 1",
+                descrizione: "Cinema capace di rappresentare un futuro che è già presente. Fantascienza, USA 1999. Durata 133 Minuti.",
                 recensione: "Non molto bello fantascienza 1",
                 stelle: 2,
                 percorsoImg: "https://images-na.ssl-images-amazon.com/images/I/91BKjFYwvoL._AC_SY879_.jpg"
             },
             {
                 titolo: "Avatar",
-                descrizione: "Descrizione fantascienza 2",
+                descrizione: "James Cameron si conferma un regista capace di fondere spettacolarità e messaggio. Fantascienza, USA, Gran Bretagna 2009. Durata 162 Minuti.",
                 recensione: "Non molto bello fantascienza 2",
                 stelle: 5,
                 percorsoImg: "https://movieplayer.net-cdn.it/images/2010/03/25/la-copertina-di-avatar-dvd-150956.jpg"
             },
             {
                 titolo: "Rogue One",
-                descrizione: "Descrizione fantascienza 3",
+                descrizione: "Un episodio indipendente che riporta in superficie senza incertezze il piacere dell'invenzione. Fantascienza, Avventura, Azione - USA 2016. Durata 133 Minuti.",
                 recensione: "Non molto bello fantascienza 3",
                 stelle: 1,
                 percorsoImg: "https://images-na.ssl-images-amazon.com/images/I/51TtZdJxXFL._SX324_BO1,204,203,200_.jpg"
             },
             {
                 titolo: "Tron - Legacy",
-                descrizione: "Descrizione fantascienza 4",
+                descrizione: "Un viaggio straniante e suggestivo in grado di comunicare ciò che è inesprimibile a parole. Fantascienza, USA 2010. Durata 127 Minuti.",
                 recensione: "Non molto bello fantascienza 4",
                 stelle: 2,
                 percorsoImg: "https://images-na.ssl-images-amazon.com/images/I/41ZaSR4KgIL._SX342_BO1,204,203,200_.jpg"
             },
             {
                 titolo: "La guerra dei mondi",
-                descrizione: "Descrizione fantascienza 5",
+                descrizione: "Fedele al racconto di H.G. Wells, un film spettacolare e favolistico firmato da Spielberg. Fantascienza, USA 2005. Durata 116 Minuti.",
                 recensione: "Non molto bello fantascienza 5",
                 stelle: 4,
                 percorsoImg: "https://img.ibs.it/images/8010773100908_0_306_0_75.jpg"
             },
             {
                 titolo: "Ready Player One",
-                descrizione: "Descrizione fantascienza 6",
+                descrizione: "Il perfetto meccanismo di intrattenimento. La fuga nel virtuale di Spielberg è un affresco vasto e onnicomprensivo quanto il Paradiso di Tintoretto. Azione, Fantascienza, Thriller - USA 2018. Durata 140 Minuti.",
                 recensione: "Non molto bello fantascienza 6",
                 stelle: 4,
                 percorsoImg: "https://m.media-amazon.com/images/I/51HalpiSqgL.jpg"
             },
             {
                 titolo: "Interstellar",
-                descrizione: "Descrizione fantascienza 7",
+                descrizione: "Uno dei film più ambiziosi degli ultimi anni in cui una vivacità narrativa e un'originalità registica fortissime costringono lo spettatore al piacere della concentrazione. Fantascienza, USA 2014. Durata 169 Minuti.",
                 recensione: "Non molto bello fantascienza 7",
                 stelle: 3,
                 percorsoImg: "https://m.media-amazon.com/images/I/51Q5MgobNsL.jpg"
             },
             {
                 titolo: "Ad Astra",
-                descrizione: "Descrizione fantascienza 8",
+                descrizione: "Un film angosciante che racconta un'ossessione e si interroga attraverso la science-fiction sull'accettazione dell'età adulta. Fantascienza, Thriller - USA, Brasile 2019. Durata 124 Minuti.",
                 recensione: "Non molto bello fantascienza 8",
                 stelle: 2,
                 percorsoImg: "https://images-na.ssl-images-amazon.com/images/I/61adO29XhSL._SY445_.jpg"
             },
             {
                 titolo: "Star Trek - Il futuro ha inizio",
-                descrizione: "Descrizione fantascienza 9",
+                descrizione: "J.J. Abrams concepisce le origini di Kirk e Spock, gira il timone del tempo e naviga verso un futuro da inventare. Fantascienza, USA 2009. Durata 127 Minuti.",
                 recensione: "Non molto bello fantascienza 9",
                 stelle: 3,
                 percorsoImg: "https://img.ibs.it/images/8010773105286_0_306_0_75.jpg"
             },
             {
                 titolo: "Blade runner",
-                descrizione: "Descrizione fantascienza 10",
+                descrizione: "Un capolavoro della storia del cinema, dove ogni elemento ha saputo comporre lo scenario di un futuro inquietante perché plausibile, dove la tecnologia non è disumana ma troppo umana. Fantascienza, USA 1982.",
                 recensione: "Non molto bello fantascienza 10",
                 stelle: 2,
                 percorsoImg: "https://i.etsystatic.com/20343000/r/il/f7a1ed/1946083803/il_570xN.1946083803_mf7q.jpg"
@@ -79,73 +79,73 @@ export let datiFilm = [
         media: [
             {
                 titolo: "Old Man &amp; The Gun",
-                descrizione: "Descrizione commedia 1",
+                descrizione: "L'ultimo film di Redford racchiude una carriera intera, i ruoli che lo hanno consacrato, la passione che non lo ha mai abbandonato. Commedia, Drammatico - USA 2018. Durata 93 Minuti.",
                 recensione: "Non molto bello commedia 1",
                 stelle: 4,
                 percorsoImg: "https://pad.mymovies.it/filmclub/2017/05/163/locandina.jpg"
             },
             {
                 titolo: "Tutto può accadere a Broadway",
-                descrizione: "Descrizione commedia 2",
+                descrizione: "Bogdanovich regala un'ora e mezza di puro divertimento, 'svitato' e cinefilo. Commedia, USA 2014. Durata 93 Minuti.",
                 recensione: "Non molto bello commedia 2",
                 stelle: 2,
                 percorsoImg: "https://pad.mymovies.it/filmclub/2014/07/173/locandina.jpg"
             },
             {
                 titolo: "Tolo Tolo",
-                descrizione: "Descrizione commedia 3",
+                descrizione: "Checco Zalone si mantiene in equilibrio sul crinale della correttezza politica, colpendo a 360°. Commedia, Italia 2020. Durata 90 Minuti.",
                 recensione: "Non molto bello commedia 3",
                 stelle: 3,
                 percorsoImg: "https://pad.mymovies.it/filmclub/2019/02/007/locandina.jpg"
             },
             {
                 titolo: "Gloria Bell",
-                descrizione: "Descrizione commedia 4",
+                descrizione: "Julianne Moore è Gloria Bell, una donna forte e fragile insieme che sa risolversi quando tutto sembra affondare, che ha la capacità di ripartire, di ritornare in pista e di danzare. Ancora. Commedia, USA 2018. Durata 102 Minuti.",
                 recensione: "Non molto bello commedia 4",
                 stelle: 1,
                 percorsoImg: "https://pad.mymovies.it/filmclub/2018/11/088/locandina.jpg"
             },
             {
-                titolo: "Gloria Bell",
-                descrizione: "Descrizione commedia 4",
+                titolo: "Chiamate un dottore!",
+                descrizione: "Una commedia lieve e garbata basata sulla simpatia e umanità dei suoi due interpreti. Commedia - Francia, 2019. Durata 90 Minuti.",
                 recensione: "Non molto bello commedia 4",
                 stelle: 4,
-                percorsoImg: "https://pad.mymovies.it/filmclub/2018/11/088/locandina.jpg"
+                percorsoImg: "https://pad.mymovies.it/filmclub/2019/12/241/locandina.jpg"
             },
             {
-                titolo: "Gloria Bell",
-                descrizione: "Descrizione commedia 4",
+                titolo: "Paradise - Una nuova vita",
+                descrizione: "Un film lunare che oscilla fra il comico, il tragico e il surreale. Un esperimento interessante. Commedia - Italia, 2020. Durata 85 Minuti.",
                 recensione: "Non molto bello commedia 4",
                 stelle: 3,
-                percorsoImg: "https://pad.mymovies.it/filmclub/2018/11/088/locandina.jpg"
+                percorsoImg: "https://pad.mymovies.it/filmclub/2019/11/133/locandina.jpg"
             },
             {
-                titolo: "Gloria Bell",
-                descrizione: "Descrizione commedia 4",
+                titolo: "Cosa resta della rivoluzione",
+                descrizione: "Una riflessione in forma di commedia sulle rivoluzioni fallite e sulle decisioni semplici ma rivoluzionarie. Commedia - Francia, 2018. Durata 88 Minuti.",
                 recensione: "Non molto bello commedia 4",
                 stelle: 5,
-                percorsoImg: "https://pad.mymovies.it/filmclub/2018/11/088/locandina.jpg"
+                percorsoImg: "https://pad.mymovies.it/filmclub/2019/03/237/locandina.jpg"
             },
             {
-                titolo: "Gloria Bell",
-                descrizione: "Descrizione commedia 4",
+                titolo: "Gli anni più belli",
+                descrizione: "Il commovente ritratto di una generazione che Muccino sa mettere a fuoco con una compiutezza senza uguali. Commedia - Italia, 2020. Durata 129 Minuti.",
                 recensione: "Non molto bello commedia 4",
                 stelle: 1,
-                percorsoImg: "https://pad.mymovies.it/filmclub/2018/11/088/locandina.jpg"
+                percorsoImg: "https://pad.mymovies.it/filmclub/2019/04/188/locandina.jpg"
             },
             {
-                titolo: "Gloria Bell",
-                descrizione: "Descrizione commedia 4",
+                titolo: "The Farewell - Una bugia buona",
+                descrizione: "Una commedia sorprendente dai toni sereni e delicati. Un inno alle collisioni di una società aperta. Commedia - USA, Cina, 2019. Durata 98 Minuti.",
                 recensione: "Non molto bello commedia 4",
                 stelle: 3,
-                percorsoImg: "https://pad.mymovies.it/filmclub/2018/11/088/locandina.jpg"
+                percorsoImg: "https://pad.mymovies.it/filmclub/2019/05/125/locandina.jpg"
             },
             {
-                titolo: "Gloria Bell",
-                descrizione: "Descrizione commedia 4",
+                titolo: "Easy living - La vita facile",
+                descrizione: "Una piccola storia che riesce a parlare di migranti senza retorica e con leggerezza. Commedia, Italia 2019. Durata 93 Minuti.",
                 recensione: "Non molto bello commedia 4",
                 stelle: 5,
-                percorsoImg: "https://pad.mymovies.it/filmclub/2018/11/088/locandina.jpg"
+                percorsoImg: "https://pad.mymovies.it/filmclub/2019/11/121/locandina.jpg"
             }
         ]
     },
@@ -154,73 +154,73 @@ export let datiFilm = [
         media: [
             {
                 titolo: "Waiting for the barbarians",
-                descrizione: "Descrizione drammatico 1",
+                descrizione: "La lotta tra due civiltà viene immortalata da una regia sontuosa ed essenziale che sente però la pressione di Hollywood . Drammatico - USA, Italia, 2019. Durata 114 Minuti.",
                 recensione: "Non molto bello drammatico 1",
                 stelle: 5,
                 percorsoImg: "https://scontent.ccdn.cloud/image/nospoiler/9e78490e-b377-46d2-b1cd-58d2c6274176/waiting-for-the-barbarians-poster-2-384x576.jpg"
             },
             {
                 titolo: "Padrenostro",
-                descrizione: "Descrizione drammatico 2",
+                descrizione: "Un bellissimo e straziante ritratto di un rapporto padre-figlio. Con un Pierfrancesco Favino magistrale. Drammatico - Italia, 2020. Durata 120 Minuti.",
                 recensione: "Non molto bello drammatico 2",
                 stelle: 2,
                 percorsoImg: "https://pad.mymovies.it/img/image/?size=400&image=https://pad.mymovies.it/cinemanews/2020/169853/locandina-ver.jpg"
             },
             {
                 titolo: "Tenet",
-                descrizione: "Descrizione drammatico 3",
+                descrizione: "Uno spy movie ambizioso e cerebrale che sfida le leggi della fisica e della messinscena. Drammatico - USA, 2020. Durata 150 Minuti.",
                 recensione: "Non molto bello drammatico 3",
                 stelle: 1,
                 percorsoImg: "https://pad.mymovies.it/filmclub/2019/10/296/locandina.jpg"
             },
             {
-                titolo: "Tenet",
-                descrizione: "Descrizione drammatico 3",
+                titolo: "Guida romantica a posti perduti",
+                descrizione: "Un road movie svagato e insieme riflessivo, che flirta con l'abisso e scandisce la frattura col passato. Drammatico - Italia, 2020. Durata 106 Minuti.",
                 recensione: "Non molto bello drammatico 3",
-                stelle: 1,
-                percorsoImg: "https://pad.mymovies.it/filmclub/2019/10/296/locandina.jpg"
+                stelle: 2,
+                percorsoImg: "https://lh3.googleusercontent.com/proxy/5Z_x06mMvguvWDjPGQVgpdeXh0V6xuypAZY61gzG6gcfZ59e4C_oBdsNqWncHhz8t6QZlyyqtDZ6UxWOVIjOJOyVuGRtyBymRNuQzMnw0TWvBQ6wCl0SV9w"
             },
             {
-                titolo: "Tenet",
-                descrizione: "Descrizione drammatico 3",
+                titolo: "Lacci",
+                descrizione: "La storia di un carnage domestico, sacrificio vano di una coppia e di una generazione post '68. Drammatico - Italia, 2020. Durata 100 Minuti.",
                 recensione: "Non molto bello drammatico 3",
-                stelle: 1,
-                percorsoImg: "https://pad.mymovies.it/filmclub/2019/10/296/locandina.jpg"
+                stelle: 4,
+                percorsoImg: "https://pad.mymovies.it/filmclub/2020/07/123/locandina.jpg"
             },
             {
-                titolo: "Tenet",
-                descrizione: "Descrizione drammatico 3",
+                titolo: "Il processo ai Chicago 7",
+                descrizione: "Una parata di star per ritrarre la controcultura dell'epoca e ricordare il valore della protesta. Drammatico - USA, 2020. Durata 129 Minuti.",
                 recensione: "Non molto bello drammatico 3",
-                stelle: 1,
-                percorsoImg: "https://pad.mymovies.it/filmclub/2019/10/296/locandina.jpg"
+                stelle: 3,
+                percorsoImg: "https://citynews.stgy.ovh/~shared/images/cinema/locandine/il-processo-ai-chicago-7-1hq6uj.jpg"
             },
             {
-                titolo: "Tenet",
-                descrizione: "Descrizione drammatico 3",
+                titolo: "Mai per sempre",
+                descrizione: "Massa intreccia 3 relazioni d'amore offrendo ad ognuna una specifica caratterizzazione. Drammatico - Italia, 2020. Durata 90 Minuti.",
                 recensione: "Non molto bello drammatico 3",
-                stelle: 1,
-                percorsoImg: "https://pad.mymovies.it/filmclub/2019/10/296/locandina.jpg"
+                stelle: 5,
+                percorsoImg: "https://pad.mymovies.it/filmclub/2020/03/019/locandina.jpg"
             }, 
             {
-                titolo: "Tenet",
-                descrizione: "Descrizione drammatico 3",
+                titolo: "Non odiare",
+                descrizione: "Un esordio che punta in alto, sonda i limiti del perdono e affida ad Alessandro Gassmann il suo ruolo più bello e viscerale. Drammatico - Italia, Polonia, 2020. Durata 96 Minuti.",
                 recensione: "Non molto bello drammatico 3",
-                stelle: 1,
-                percorsoImg: "https://pad.mymovies.it/filmclub/2019/10/296/locandina.jpg"
+                stelle: 5,
+                percorsoImg: "https://pad.mymovies.it/filmclub/2019/09/045/locandina.jpg"
             },
             {
-                titolo: "Tenet",
-                descrizione: "Descrizione drammatico 3",
+                titolo: "Assandira",
+                descrizione: "Una storia raccontata con passione, dolore ed energia. Drammatico - Italia, 2020. Durata 128 Minuti.",
                 recensione: "Non molto bello drammatico 3",
-                stelle: 1,
-                percorsoImg: "https://pad.mymovies.it/filmclub/2019/10/296/locandina.jpg"
+                stelle: 4,
+                percorsoImg: "https://pad.mymovies.it/filmclub/2020/07/126/locandina.jpg"
             },
             {
-                titolo: "Tenet",
-                descrizione: "Descrizione drammatico 3",
+                titolo: "Favolacce",
+                descrizione: "Una favola tanto nera quanto vera. Perché i draghi dell'amoralità si nascondono dove meno ci si aspetta. Drammatico - Italia, 2020. Durata 98 Minuti.",
                 recensione: "Non molto bello drammatico 3",
                 stelle: 1,
-                percorsoImg: "https://pad.mymovies.it/filmclub/2019/10/296/locandina.jpg"
+                percorsoImg: "https://pad.mymovies.it/filmclub/2019/01/142/locandina.jpg"
             }
         ]
     }
